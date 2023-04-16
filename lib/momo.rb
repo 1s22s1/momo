8ab29dc7881dad0d14a73711d21c8f3104fd9f2b
1s22s1 <1s22s1@example.com>
2023-04-16 19:04:34 +0900

    First commit

---
+++ lib/momo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'momo/version'
+
+module Momo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/momo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Momo do
+  it 'has a version number' do
+    expect(Momo::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
